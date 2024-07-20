fcff6491a814922ba5206180546bb9ef920ffd03
AhmedSoliman00 <himozhima@gmail.com>
2024-07-20 03:08:13 +0300

    renaming file

---
+++ api/controllerss/createListing.controller.js
@@ -0,0 +1,17 @@
+import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    console.log(listing)
+    return res.status(201).json({
+      success: true,
+      statusCode: 201,
+      message: "Listing created successfully",
+      data: listing,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
