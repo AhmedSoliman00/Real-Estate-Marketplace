69364d4e1b58d3437f22b9d7affb470c064be6b4
AhmedSoliman00 <himozhima@gmail.com>
2024-07-20 03:24:27 +0300

    update listing api

--- api/controllerss/createListing.controller.js
+++ api/controllerss/createListing.controller.js
@@ -1,17 +1,43 @@
-import Listing from "../models/listing.model.js";
-import { errorHandler } from "../utils/error.js";
+import Listing from '../models/listing.model.js'
+import { errorHandler } from '../utils/error.js'
 export const createListing = async (req, res, next) => {
   try {
-    const listing = await Listing.create(req.body);
+    const listing = await Listing.create(req.body)
     console.log(listing)
     return res.status(201).json({
       success: true,
       statusCode: 201,
-      message: "Listing created successfully",
+      message: 'Listing created successfully',
       data: listing,
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findByIdAndUpdate(req.params.id)
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found'))
+    }
+    if (req.user.id !== listing.userRef) {
+      return next(
+        errorHandler(401, 'You are not authorized to update this listing')
+      )
+    }
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true } // return the updated document
+    )
+    return res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: 'Listing updated successfully',
+      data: updatedListing,
+    })
+  } catch {
+    next(error)
+  }
+}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createListing } from "../controllerss/createListing.controller.js";
+import { createListing,updateListing } from "../controllerss/createListing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create",verifyToken ,createListing);
+router.put("/update/:id",verifyToken ,updateListing);
 
 export default router;
